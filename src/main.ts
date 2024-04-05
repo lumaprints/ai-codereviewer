5c3f8abc59943348ebc5ce7ac57ba316454b0ca7
Raymond De Castro <36554808+rgdcastro@users.noreply.github.com>
2024-04-05 11:27:02 +0800

    Update main.ts

--- src/main.ts
+++ src/main.ts
@@ -81,6 +81,8 @@ async function analyzeCode(
 function createPrompt(file: File, chunk: Chunk, prDetails: PRDetails): string {
   return `Your task is to review pull requests. Instructions:
 - Provide the response in following JSON format:  {"reviews": [{"lineNumber":  <line_number>, "reviewComment": "<review comment>"}]}
+- Check if best practices is followed
+- Check if there are possible security concerns
 - Do not give positive comments or compliments.
 - Provide comments and suggestions ONLY if there is something to improve, otherwise "reviews" should be an empty array.
 - Write the comment in GitHub Markdown format.
